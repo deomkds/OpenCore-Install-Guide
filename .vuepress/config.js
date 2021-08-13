3be990802326a26ef96260868dfff481548116db
Marcus Vinícius <mv.cunha2009@gmail.com>
2021-08-12 23:02:50 -0300

    Configura o VuePress.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -19,7 +19,7 @@ module.exports = {
         }],
         ["link", {
             rel: "'stylesheet",
-            href: "/styles/website.css"
+            href: "styles/website.css"
         },]
     ],
     base: '/OpenCore-Install-Guide/',
